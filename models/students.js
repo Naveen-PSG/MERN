1bf6f170c99d48c663609681bf49070ca014a2db
22mx210 <22mx210@MCA.PSG>
2023-05-11 18:39:07 +0530

    Test

---
+++ models/students.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 33,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  enrollnumber: {
+    type: Number,
+    min: 1,
+    max: 130
+  },
+  phonenumber: {
+    type: Number
+    // min: 1,
+    // max: 130
+  }
+});
+
+module.exports = mongoose.model('students', studentSchema);
+
+
+
